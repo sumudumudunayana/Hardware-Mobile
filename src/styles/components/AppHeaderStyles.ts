3c67e3f41faa257830bcfe75302fbf64018da4f1
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-24 17:26:09 +0530

    app header styles added

---
+++ src/styles/components/AppHeaderStyles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+
+  backButton: {
+    padding: 4,
+  },
+
+  icon: {
+    fontSize: 26,
+    color: "#0f172a",
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#0f172a",
+  },
+
+  spacer: {
+    width: 26,
+  },
+});
