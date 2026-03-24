44f32a241119bdfe9f7e0533ef15fff94f400e1f
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-24 17:28:47 +0530

    product edit screen updated

---
+++ src/screens/products/ProductEditScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function ProductEditScreen() {
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <Text style={{ fontSize: 22 }}>Add / Edit Product</Text>
+    </View>
+  );
+}
