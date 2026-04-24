ae5994df278b247fe14f42072e8297d667e3a044
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-24 12:43:24 +0530

    app header updated

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import styles from "../styles/components/AppHeaderStyles";
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from '../styles/components/AppHeaderStyles';
 
 export default function AppHeader({
   title,
