c5c2a4339a0eeda17e0fedef1b493f55f53e3ae0
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-04-24 12:44:24 +0530

    category details screen updated

--- src/screens/category/CategoryDetailsScreen.tsx
+++ src/screens/category/CategoryDetailsScreen.tsx
@@ -1,68 +1,39 @@
-import React from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  ScrollView,
-} from "react-native";
+import React from 'react';
+import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
 
-import AppHeader from "../../components/AppHeader";
-import styles from "../../styles/category/CategoryDetailsScreenStyles";
+import AppHeader from '../../components/AppHeader';
+import styles from '../../styles/category/CategoryDetailsScreenStyles';
 
-export default function CategoryDetailsScreen({
-  route,
-  navigation,
-}: any) {
-  const { category } = route.params;
+export default function CategoryDetailsScreen({route, navigation}: any) {
+  const {category} = route.params;
 
   return (
     <View style={styles.container}>
-      <AppHeader
-        title="Category Details"
-        onBack={() => navigation.goBack()}
-      />
+      <AppHeader title="Category Details" onBack={() => navigation.goBack()} />
 
       <ScrollView
-        contentContainerStyle={
-          styles.scrollContent
-        }
-        showsVerticalScrollIndicator={false}
-      >
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}>
         <View style={styles.card}>
-          <Text style={styles.title}>
-            Category Information
-          </Text>
+          <Text style={styles.title}>Category Information</Text>
 
           {/* CATEGORY ID */}
           <View style={styles.infoRow}>
-            <Text style={styles.label}>
-              Category ID
-            </Text>
-            <Text style={styles.value}>
-              {category.categoryId}
-            </Text>
+            <Text style={styles.label}>Category ID</Text>
+            <Text style={styles.value}>{category.categoryId}</Text>
           </View>
 
           {/* CATEGORY NAME */}
           <View style={styles.infoRow}>
-            <Text style={styles.label}>
-              Category Name
-            </Text>
-            <Text style={styles.value}>
-              {category.categoryName}
-            </Text>
+            <Text style={styles.label}>Category Name</Text>
+            <Text style={styles.value}>{category.categoryName}</Text>
           </View>
 
           {/* DESCRIPTION */}
           <View style={styles.infoRow}>
-            <Text style={styles.label}>
-              Description
-            </Text>
-            <Text style={styles.value}>
-              {category.categoryDescription}
-            </Text>
+            <Text style={styles.label}>Description</Text>
+            <Text style={styles.value}>{category.categoryDescription}</Text>
           </View>
-
         </View>
       </ScrollView>
     </View>
