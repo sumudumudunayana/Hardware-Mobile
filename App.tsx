9446e01eefa373c4f91d67e66fe96f0f61c2d2c6
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-05-03 22:25:36 +0530

    tost messages added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { AuthProvider } from "./src/context/AuthContext";
+import Toast from "react-native-toast-message";
 
 export default function App() {
   return (
     <AuthProvider>
       <AppNavigator />
+      <Toast topOffset={60} />
     </AuthProvider>
   );
 }
