a3fc8dbed9a09b06a5b14ffac178fb227e894f7d
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-05-06 23:07:29 +0530

    api updated

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: "https://hardware-backend-express.onrender.com/api",
+  baseURL: 'https://hardware-backend-express.onrender.com/api',
   // baseURL: "http://10.0.2.2:5500/api",
   timeout: 60000,
 });
 
 // Attach JWT automatically
 api.interceptors.request.use(
-  async (config) => {
-    const token = await AsyncStorage.getItem("token");
+  async config => {
+    const token = await AsyncStorage.getItem('token');
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -18,9 +18,9 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => {
+  error => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
