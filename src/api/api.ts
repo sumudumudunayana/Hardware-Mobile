2c2580b20f68f338520505e30ed2527358f6843b
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-05-06 23:13:33 +0530

    api updated

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,11 +11,9 @@ const api = axios.create({
 api.interceptors.request.use(
   async config => {
     const token = await AsyncStorage.getItem('token');
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   },
   error => {
