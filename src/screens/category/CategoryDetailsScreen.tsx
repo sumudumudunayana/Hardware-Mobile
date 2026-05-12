c5fbd2f84b701d0a469ca90fdd7703c6160799dd
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-05-12 20:51:32 +0530

    category details screen updated

--- src/screens/category/CategoryDetailsScreen.tsx
+++ src/screens/category/CategoryDetailsScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
-
 import AppHeader from '../../components/AppHeader';
 import styles from '../../styles/category/CategoryDetailsScreenStyles';
 
@@ -10,7 +9,6 @@ export default function CategoryDetailsScreen({route, navigation}: any) {
   return (
     <View style={styles.container}>
       <AppHeader title="Category Details" onBack={() => navigation.goBack()} />
-
       <ScrollView
         contentContainerStyle={styles.scrollContent}
         showsVerticalScrollIndicator={false}>
