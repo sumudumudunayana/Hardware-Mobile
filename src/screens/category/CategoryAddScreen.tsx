0feac6fe41a4553ac89acfdd3359b68c7a5b466a
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-05-18 22:18:45 +0530

    category add screnn updated

--- src/screens/category/CategoryAddScreen.tsx
+++ src/screens/category/CategoryAddScreen.tsx
@@ -8,7 +8,6 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-
 import Toast from 'react-native-toast-message';
 import api from '../../api/api';
 import AppHeader from '../../components/AppHeader';
