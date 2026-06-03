4895aca6f3c2df5f965c1c71c7342c4142677853
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-06-03 19:27:10 +0530

    customer edit screen updated

--- src/screens/customer/CustomerEditScreen.tsx
+++ src/screens/customer/CustomerEditScreen.tsx
@@ -107,7 +107,6 @@ export default function CustomerEditScreen({route, navigation}: any) {
           text1: 'Session Expired',
           text2: 'Please login again',
         });
-
         navigation.replace('Login');
         return;
       }
